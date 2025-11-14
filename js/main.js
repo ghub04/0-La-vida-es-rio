f2f39c15cf1d907edc564232f3bef199f53dfe51
Guillermo <castilloaguilera7@gmail.com>
2025-11-14 19:36:26 +0100

    Cargar las citas con id

--- js/main.js
+++ js/main.js
@@ -1,35 +1,16 @@
 import { loadingBar } from '/js/cover.js';
 import { textAnimation } from '/js/cover.js';
 import { customCursor } from '/js/cursor.js';
+import { quoteLoad } from '/js/quoteLoad.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     textAnimation()
     loadingBar()
     customCursor()
+    quoteLoad()
 
-    // quotes
-    fetch("/js/quotes.json")
-        .then(response => response.json())
-        .then(data => {
-            // Aquí ya tienes las citas cargadas
-            console.log("Citas cargadas:", data);
-            mostrarCitas(data);
-        })
-        .catch(error => console.error("Error al cargar las citas:", error));
 
-    function mostrarCitas(quotes) {
-        const container = document.querySelector('.quotes-container')
 
-        quotes.forEach(quote => {
-            const quoteElement = document.createElement('div')
-            quoteElement.classList.add('quote')
-
-            quoteElement.innerHTML = `
-      <p class="quote-text">"${quote.text}"</p>`;
-
-            container.appendChild(quoteElement);
-        })
-    }
-
+    //ends
 });
 
