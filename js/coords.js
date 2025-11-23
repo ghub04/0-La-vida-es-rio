3fbbdce6484d5255a2af95b7314f034641f77aff
Guillermo <castilloaguilera7@gmail.com>
2025-11-23 12:53:00 +0100

    ubicaciones y animacion de estas

--- js/coords.js
+++ js/coords.js
@@ -6,9 +6,12 @@ scale:1,
 transformOrigin:"50% 50%",
 })
 
-gsap.to(location,{
-  scale:1.2,
-  repeat:-1,
-  yoyo:true,
-  duration:1,
+location.forEach((loc,i) =>{
+  gsap.to(loc,{
+    scale:1.5,
+    repeat:-1,
+    yoyo:true,
+    duration:1,
+    delay:i*.2,
+  })
 })
