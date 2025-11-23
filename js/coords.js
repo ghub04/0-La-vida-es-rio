914e0e4f45860d87e1b27a7b87fe2e8ff66443fe
Guillermo <castilloaguilera7@gmail.com>
2025-11-23 22:16:27 +0100

    texto oceanos

--- js/coords.js
+++ js/coords.js
@@ -1,17 +1,16 @@
-const location=document.querySelectorAll('.circle')
-console.log('coords')
+const location = document.querySelectorAll('.circle')
 
-gsap.set(location,{
-scale:1,
-transformOrigin:"50% 50%",
+gsap.set(location, {
+  scale: 1,
+  transformOrigin: "50% 50%",
 })
 
-location.forEach((loc,i) =>{
-  gsap.to(loc,{
-    scale:1.5,
-    repeat:-1,
-    yoyo:true,
-    duration:1,
-    delay:i*.2,
+location.forEach((loc, i) => {
+  gsap.to(loc, {
+    scale: 1.5,
+    repeat: -1,
+    yoyo: true,
+    duration: 1,
+    delay: i * .2,
   })
 })
