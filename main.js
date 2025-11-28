fd2feb17174b5826f12cdcc707a857c7d30ee4e5
Guillermo <castilloaguilera7@gmail.com>
2025-11-28 21:08:16 +0100

    test cambio rutas relativas

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { customCursor } from "js / cursor.js";
-import { quoteLoad } from "js/quoteLoad.js";
-import "js/timelineCover.js";
-import "js/coords.js";
-import "js/aboutLoad.js"
-import "js/mapDrag.js"
+import { customCursor } from "./js/cursor.js";
+import { quoteLoad } from "./js/quoteLoad.js";
+import "./js/timelineCover.js";
+import "./js/coords.js";
+import "./js/aboutLoad.js"
+import "./js/mapDrag.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   customCursor();
