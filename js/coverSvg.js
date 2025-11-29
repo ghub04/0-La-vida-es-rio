4e5b12f8339e314a5c480438d7162a9fd58c6f5c
Guillermo <castilloaguilera7@gmail.com>
2025-11-29 17:10:11 +0100

    cambio colores del mapa

--- js/coverSvg.js
+++ js/coverSvg.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(DrawSVGPlugin)
     console.log('draw-svg')
 
+    const river = document.querySelector('#svg-river path')
 
+    let tlSvg = gsap.timeline()
 
+    gsap.set(river, {
+        drawSVG: '0'
+    })
 
-
-
-
+    tlSvg.to(river, {
+        drawSVG: '0% 50%',
+        duration: 10,
+    })
 
     // gsap code here!
 });
