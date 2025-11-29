c31125274cae34a698ef3348d48ccc00da49af24
Guillermo <castilloaguilera7@gmail.com>
2025-11-29 17:47:36 +0100

    eliminado svg cover y js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 import { customCursor } from "./cursor.js";
 import { quoteLoad } from "./quoteLoad.js";
-import "./timelineCover.js";
+// import "./timelineCover.js";
 import "./coords.js";
 import "./aboutLoad.js"
 import "./mapDrag.js"
-import "./coverSvg.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   customCursor();
