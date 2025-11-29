7af5d5d46a636444e88edaf2da43182e41588c76
Guillermo <castilloaguilera7@gmail.com>
2025-11-29 17:17:57 +0100

    cambio efectos orilla

--- js/coords.js
+++ js/coords.js
@@ -23,8 +23,8 @@ const shoreWaves = document.querySelector('.shore')
 let tlShore = gsap.timeline()
 
 tlShore.to(shoreWaves, {
-  autoAlpha: 0.8,
+  autoAlpha: .2,
   repeat: -1,
   yoyo: true,
-  duration: 5
+  duration: 4,
 })
