415a2132106c7800a63e1100a9ef4d366a523372
Guillermo <castilloaguilera7@gmail.com>
2025-11-30 12:05:37 +0100

    añadido coverStyle css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { customCursor } from "./cursor.js";
 import { quoteLoad } from "./quoteLoad.js";
-// import "./timelineCover.js";
+import "./timelineCover.js";
 import "./coords.js";
 import "./aboutLoad.js"
 import "./mapDrag.js"
