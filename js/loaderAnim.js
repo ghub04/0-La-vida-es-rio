33597e579529ca7128d4ceb70f44d93b85c93fcc
Guillermo <castilloaguilera7@gmail.com>
2025-12-02 20:23:20 +0100

    animacion de entrada

--- js/loaderAnim.js
+++ js/loaderAnim.js
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const border = document.querySelector('.border-container')
     const waves = document.querySelector('.waves')
     const info = document.querySelector('.info-about')
+    const cover = document.querySelector(".cover");
+    const closeCover = document.querySelector('#close-cover')
+
+    // AUDIO
+    const audio = new Audio('assets/forest-wind.mp3')
+    const audioToggle = document.querySelector('.audio-btn')
+    audio.loop = true
+
 
     const titleContainer = document.querySelector('.title')
     const titleH1 = document.querySelector('.title h1')
@@ -27,18 +35,21 @@ document.addEventListener("DOMContentLoaded", () => {
         drawSVG: "0%"
     });
 
+
+
     // 3) Animar el trazado
     tlCover.to(svgPath, {
         autoAlpha: 1,
         drawSVG: "100%",
         duration: 3,
         ease: "power2.out",
-        delay: 0.5,
+        delay: 2,
         onComplete: () => {
             // 4) Fade out del loader
             loader.classList.add("fade-out");
         }
-    }).call(textAnimation)
+    })
+        .call(textAnimation, null, '-=0.5')
 
         // 5) Mostrar el infoWrapper
         .to(infoWrapper, {
@@ -48,7 +59,8 @@ document.addEventListener("DOMContentLoaded", () => {
             ease: "power2.out",
             delay: 0.2,
 
-        });
+        }).call(splitInfoAnim, null, '-=1')
+        .call(closeCoverAnim, null, '+=0.2')
 
     // anim waves
     function loaderWaves() {
@@ -60,13 +72,41 @@ document.addEventListener("DOMContentLoaded", () => {
             {
                 scale: 2.4,
                 autoAlpha: 0,
-                duration: 1.6,
+                duration: 2,
                 ease: "power1.out",
                 repeat: -1
             }
         );
     }
     loaderWaves()
+
+    //close Cover
+    function closeCoverAnim() {
+
+        gsap.set(closeCover, {
+            y: 0,
+            autoAlpha: 1,
+        });
+
+        function hideCover() {
+            gsap.to(cover, {
+                autoAlpha: 0,
+                duration: 0.5,
+            });
+        }
+
+        closeCover.addEventListener("click", () => {
+            // console.log("close");
+            hideCover();
+            // mapText();
+
+            if (!isAudioOn) {
+                audio.play()
+                isAudioOn = true
+            }
+        });
+    }
+    // closeCoverAnim()
 
 
     // info text anim
@@ -86,70 +126,15 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    //la vida es rio anim
-    // ANIMACION TITLE
-    // gsap.set(titleContainer, {
-    //     autoAlpha: 0,
-    //     y: '30'
-    // })
-
-    // const f = document.querySelector('#wavy feTurbulence');
-    // function textAnimation() {
-    //     console.log('title')
-    //     document.fonts.ready.then(() => {
-    //         requestAnimationFrame(() => {
-    //             f.baseFrequency += '0.005';
-    //         });
-
-    //         titleH1.offsetHeight;
-    //         setTimeout(() => {
-
-
-    //             let split = SplitText.create(titleH1, {
-    //                 type: "chars, words",
-    //             });
-    //             let textAnimation = gsap.timeline();
-    //             let words = split.words
-
-    //             textAnimation.to(titleH1, {
-    //                 delay: 1,
-    //                 y: 0,
-    //                 autoAlpha: 1,
-    //             })
-    //                 .to(titleContainer, {
-    //                     y: 0,
-    //                     autoAlpha: 1,
-    //                     duration: 3,
-    //                     ease: "power2.out",
-    //                 }).from(words, {
-    //                     y: 100,
-    //                     duration: 3,
-    //                     stagger: 0.05,
-    //                 }, '<')
-    //                 .to(f,
-    //                     {
-    //                         attr: { baseFrequency: "0.006" },
-    //                         duration: 20,
-    //                         stagger: 0.5,
-    //                         repeat: -1,
-    //                         yoyo: true,
-    //                     },
-    //                     "<"
-    //                 );
-    //         }, 50)
-    //     });
-    // }
-    // -------
-
-    // 
+    // TITULO ANIM
     const displacementMap = document.querySelector('#wavy feTurbulence');
 
-    // 
+
     gsap.set(displacementMap, {
-        attr: { baseFrequency: 0.001 } // Valor inicial bajo pero válido
+        attr: { baseFrequency: 0.003 } // Valor inicial 
     });
 
-    // 
+
     gsap.set(titleContainer, {
         autoAlpha: 0,
         y: 30
@@ -198,7 +183,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }, '<');
         });
     }
-
+    // -----
 
 
 
