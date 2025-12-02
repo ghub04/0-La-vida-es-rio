67079967648991b8d774eab3c17af12382a6e94f
Guillermo <castilloaguilera7@gmail.com>
2025-12-02 18:31:39 +0100

    waves anim en gsap

--- js/loaderAnim.js
+++ js/loaderAnim.js
@@ -2,22 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(DrawSVGPlugin);
 
     const loader = document.querySelector("#loader");
-    const cover = document.querySelector(".info-wrapper");
+    const infoWrapper = document.querySelector(".info-wrapper");
     const mapSvg = document.querySelector(".loader-container");
     const mapPath = document.querySelector(".loader-container .loader-path");
+    const border = document.querySelector('.border-container')
+    const waves = document.querySelector('.waves')
 
     // 1) Mostrar el SVG (estaba a opacity 0)
-    gsap.to(mapSvg, {
-        autoAlpha: 1,
-        duration: 0.5,
-        delay: 0.3
-    });
+    // gsap.to(mapSvg, {
+    //     autoAlpha: 1,
+    //     duration: 0.5,
+    //     delay: 0.3
+    // });
 
     // 2) Preparar el trazo del mapa
-    gsap.set(mapPath, { drawSVG: "0%" });
+    gsap.set(mapPath, {
+        drawSVG: "0%"
+    });
 
     // 3) Animar el trazado
     gsap.to(mapPath, {
+        autoAlpha: 1,
         drawSVG: "100%",
         duration: 3,
         ease: "power2.out",
@@ -26,8 +31,8 @@ document.addEventListener("DOMContentLoaded", () => {
             // 4) Fade out del loader
             loader.classList.add("fade-out");
 
-            // 5) Mostrar el cover
-            gsap.to(cover, {
+            // 5) Mostrar el infoWrapper
+            gsap.to(infoWrapper, {
                 autoAlpha: 1,
                 duration: 1,
                 ease: "power2.out",
@@ -35,4 +40,25 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         }
     });
+
+    function loaderWaves() {
+        gsap.fromTo(waves,
+            {
+                scale: 0.6,
+                opacity: 0.9
+            },
+            {
+                scale: 2.4,
+                opacity: 0,
+                duration: 1.6,
+                ease: "power1.out",
+                repeat: -1
+            }
+        );
+    }
+    loaderWaves()
+
+
+
+    // gsap ends
 });
